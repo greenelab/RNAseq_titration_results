ab888cddec48e1d7017612f50f2a645fcac577d2
Jaclyn Taroni <jaclyn.n.taroni@gmail.com>
2016-07-29 12:12:52 -0400

    add command line argument for number of repeats of pipeline

--- classifier_repeat_wrapper.R
+++ classifier_repeat_wrapper.R
@@ -1,14 +1,22 @@
 # J. Taroni Jul 2016
-# The purpose of this script is to read in TGCA array and sequencing data,
-# to preprocess leaving only overlapping genes and samples with complete 
-# subtype information, and to split the data intotraining and testing sets
+# This script is a wrapper for running the BRCA subtype pipeline repeatedly with
+# different random seeds.
 # It should be run from the command line.
-# USAGE: Rscript classifier_repeat_wrapper.R
+# USAGE: Rscript classifier_repeat_wrapper.R <n.repeats>
+
+args <- commandArgs(trailingOnly = TRUE)
+n.repeats <- as.integer(args[1])
+if (is.na(n.repeats)) {
+  message("Number of repeats set to default: 10")
+  n.repeats <- 10
+} else {
+  message(paste("Number of repeats set to", n.repeats))
+}
 
 kInitialSeed <- 12
 set.seed(kInitialSeed)
 
-seeds <- sample(1:10000, 10)
+seeds <- sample(1:10000, n.repeats)
 
 for(seed in seeds){
   system(paste("Rscript run_experiments.R", seed))
