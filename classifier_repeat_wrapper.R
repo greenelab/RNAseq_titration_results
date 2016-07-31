d6e7843c022557b3a4943dcc39ee458f0101573e
jaclyn-taroni <jaclyn.n.taroni@gmail.com>
2016-07-31 13:53:13 -0400

    change output messages for running repeated pipeline

--- classifier_repeat_wrapper.R
+++ classifier_repeat_wrapper.R
@@ -7,10 +7,10 @@
 args <- commandArgs(trailingOnly = TRUE)
 n.repeats <- as.integer(args[1])
 if (is.na(n.repeats)) {
-  message("Number of repeats set to default: 10")
+  message("\nNumber of repeats set to default: 10")
   n.repeats <- 10
 } else {
-  message(paste("Number of repeats set to", n.repeats))
+  message(paste("\nNumber of repeats set to", n.repeats))
 }
 
 kInitialSeed <- 12
@@ -18,8 +18,11 @@ set.seed(kInitialSeed)
 
 seeds <- sample(1:10000, n.repeats)
 
+rep.count <- 1
 for(seed in seeds){
+  message(paste("\n\n#### REPEAT NUMBER", rep.count, "####\n\n"))
   system(paste("Rscript run_experiments.R", seed))
+  rep.count <- rep.count + 1
 }
 
 system("Rscript 3-plot_subtype_kappa.R")
