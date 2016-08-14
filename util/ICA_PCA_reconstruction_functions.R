db422ea29e2cac149cd118732f5acd3c442b0bf0
jaclyn-taroni <jaclyn.n.taroni@gmail.com>
2016-08-14 12:15:42 -0400

    add functions for ICA/PCA reconstruction - training reduced dimensionality space and test reconstruction from training space, evaluate error

--- util/ICA_PCA_reconstruction_functions.R
+++ util/ICA_PCA_reconstruction_functions.R
@@ -51,3 +51,158 @@ GetMASE <- function(true.mat, recon.mat){
   return(mase)
 
 }
+
+ExpressionDataTableToMatrixT <- function(dataTable){
+  # This function transposes a data.table such that then samples are rows,
+  # columns are genes
+  # 
+  # Args:
+  #   dataTable: data.table where the first column contains gene identifiers,
+  #              columns are samples
+  #              
+  # Returns:
+  #   data.mat: transposed data matrix
+  # 
+  require(data.table)
+  data.mat <- data.frame(dataTable[, 2:ncol(dataTable), with=F])
+  data.mat <- apply(data.mat, 2, as.numeric)
+  # include gene identifiers
+  rownames(data.mat) <- dataTable[[1]]
+  return(t(data.mat))  # transpose
+}
+
+EvaluatePCARecon <- function(train.dt, test.dt, no.comp = 50){
+  # This function takes training and test set data.tables
+  # performs PCA on training, projects test data into training PC space and 
+  # back out - quantifies 'reconstruction error' using MASE
+  # 
+  # Args:
+  #   train.dt: a data.table of training data
+  #   test.dt: a data.table of test data
+  #   no.comp: number of principal components to use for reconstruction
+  # 
+  # Returns:
+  #   A list with the values
+  #     MASE: mean(mean absolute scaled error between test values and 
+  #                reconstructed test values, per gene basis)
+  #     PC: the PrcompDataTable output (principal components analysis 
+  #         on the training data)
+  #    
+  require(data.table)
+  
+  train.is.dt <- "data.table" %in% class(train.dt)
+  test.is.dt <- "data.table" %in% class(test.dt)
+  any.not.dt <- !(any(c(train.is.dt, test.is.dt)))
+  if (any.not.dt) {
+    stop("train.dt and test.dt must both be data.tables")
+  }
+  if (!(all(train.dt[[1]] %in% test.dt[[1]]))) {
+    stop("Gene identifiers in data.tables must match")
+  }
+  
+  PrcompDataTable <- function(dataTable){
+    # perform PCA on a data.table of gene expression values 
+    # where genes are rows and columns are samples
+    mat.t <- ExpressionDataTableToMatrixT(dataTable)
+    pca <- prcomp(mat.t)
+    return(pca)
+  }
+  
+  PCNewDataTable <- function(pc, dataTable, no.comp){
+    # Reconstruct test expression data using training PC space
+    # Error-handling 
+    if (class(pc) != "prcomp"){
+      stop("pc must be prcomp object")
+    }
+    # get new data (dataTable) into correct form (matrix, transposed)
+    mat.t <- ExpressionDataTableToMatrixT(dataTable)
+    proj.test <- scale(mat.t, pc$center, pc$scale) %*% pc$rotation[, 1:no.comp] 
+    recon.test <- proj %*% t(pc$rotation[, 1:no.comp])
+    return(recon.test)
+  }
+  
+  # PCA on training data
+  pc <- PrcompDataTable(train.dt)
+  # project test data into training PC space & back out
+  recon.test <- PCNewDataTable(pc, test.dt, no.comp)
+  # transpose mat for test data, to be used as "true.mat"
+  test.dm.t <- ExpressionDataTableToMatrixT(test.dt)
+  
+  
+  # quantification: how well did we do at reconstruction?
+  mase <- GetMASE(true.mat = test.dm.t, recon.mat = recon.test)
+  names(mase) <- colnames(test.dm.t)
+  
+  # return reconstruction error and PCA output
+  return(list("MASE" = mase, "PC" = pc))
+  
+  
+}
+
+EvaluateICARecon <- function(train.dt, test.dt, no.comp = 50){
+  # This function takes training and test set data.tables
+  # performs ICA on training, projects test data into PC space, then IC space
+  # and back out - quantifies 'reconstruction error' using MASE
+  # 
+  # Args:
+  #   train.dt: a data.table of training data
+  #   test.dt: a data.table of test data
+  #   no.comp: number of independent components to use
+  # 
+  # Returns:
+  #   A list with the values
+  #     MASE: mean(mean absolute scaled error between test values and 
+  #                reconstructed test values, per gene basis)
+  #     IC: the FastICADataTable output (independent components analysis 
+  #         on the training data)
+  #   
+  require(data.table)
+  # Error-handling
+  train.is.dt <- "data.table" %in% class(train.dt)
+  test.is.dt <- "data.table" %in% class(test.dt)
+  any.not.dt <- !(any(c(train.is.dt, test.is.dt)))
+  if (any.not.dt) {
+    stop("train.dt and test.dt must both be data.tables")
+  }
+  if (!(all(train.dt[[1]] %in% test.dt[[1]]))) {
+    stop("Gene identifiers in data.tables must match")
+  }
+  
+  FastICADataTable <- function(dataTable, n.comp){
+    # perform ICA on a data.table of gene expression values 
+    # where genes are rows and columns are samples
+    mat.t <- ExpressionDataTableToMatrixT(dataTable)
+    ica <- fastICA::fastICA(mat.t, n.comp = no.comp, alg.typ = "parallel",
+                            fun = "exp", method = "C")
+    return(ica)
+  }
+  
+  ICNewDataTable <- function(ic, dataTable){
+    # Reconstruct test expression data using training IC space
+    # Error-handling
+    # Are the correct matrices in the list ic
+    correct.matrices <- all(names(ic) == c("X", "K", "W", "A", "S"))
+    if (!(correct.matrices)) {
+      stop("ic must be output of fastICA::fastICA")
+    }
+    # get new data (dataTable) into correct form (matrix, transposed)
+    mat.t <- ExpressionDataTableToMatrixT(dataTable)
+    whiten.mat <- (scale(mat.t, center=TRUE, scale=FALSE) %*% ic$K )
+    recon.mat <- (whiten.mat %*% ic$W %*% ic$A)
+    return(recon.mat)
+  }
+  
+  # ICA on training data
+  ic <- FastICADataTable(train.dt, no.comp)
+  # project test data into training IC space & back out
+  recon.test <- ICNewDataTable(ic, test.dt)
+  # transpose mat for test data, to be used as "true.mat"
+  test.dm.t <- ExpressionDataTableToMatrixT(test.dt)
+  
+  # quantification: how well did we do at reconstruction?
+  mase <- GetMASE(true.mat = test.dm.t, recon.mat = recon.test)
+  
+  # return reconstruction error and ICA output
+  return(list("MASE" = mase, "IC" = ic))
+  
+}
