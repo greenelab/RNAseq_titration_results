ad7e0cfb9cb38a1c9e1ec5c05427bbf267658b71
Jaclyn Taroni <jaclyn.n.taroni@gmail.com>
2017-02-08 14:30:24 -0500

    address comments PR #10
    build design matrices with model.matrix, move cont.matrix outside of
    logic, and move GetFiteBayes outside of GetFiteBayesList (because it
    will be utilized in small n experiments)

--- util/differential_expression_functions.R
+++ util/differential_expression_functions.R
@@ -1,3 +1,5 @@
+source(file.path("util", "train_test_functions.R"))
+
 GetDesignMatrices <- function(norm.list, sample.df, subtype = "Basal") {
   # This function takes a list of normalized expression data and returns
   # a list of design matrices to be used to detect differentially expressed 
@@ -29,21 +31,20 @@ GetDesignMatrices <- function(norm.list, sample.df, subtype = "Basal") {
   seq.lvls <- names(norm.list)
   for (amt.seq in seq.lvls) {
     
+    # get a factor vector of labels, ordered to match the columns of
+    # the amt.seq under consideration
+    group.factor <- 
+      as.character(GetOrderedSubtypeLabels(norm.list[[amt.seq]]$log,
+                                           sample.df))
+    group.factor[which(group.factor != subtype)] <- "Other"
+    group.factor <- as.factor(group.factor)
+    
     # build design matrix
-    design.mat <- matrix(0, nrow = (ncol(norm.list[[amt.seq]]$log) - 1), 
-                         ncol = 2)
-    colnames(design.mat) <- c(subtype, "Other")
+    design.mat <- model.matrix(~0 + group.factor)
+    colnames(design.mat) <- levels(group.factor)
     rownames(design.mat) <- 
       colnames(norm.list[[amt.seq]]$log)[2:ncol(norm.list[[amt.seq]]$log)]
-    
-    design.mat[which(rownames(design.mat) %in% 
-                       sample.df$sample[which(sample.df$subtype == subtype)]), 
-               subtype] <- 1
-    
-    design.mat[which(rownames(design.mat) %in% 
-                         sample.df$sample[which(sample.df$subtype != subtype)]), 
-               "Other"] <- 1
-    
+  
     # add to list
     design.list[[amt.seq]] <- design.mat
     
@@ -52,6 +53,72 @@ GetDesignMatrices <- function(norm.list, sample.df, subtype = "Basal") {
   return(design.list)
   
 }
+
+GetFiteBayes <- function(exprs, design.mat) {
+  # function for performing differential expression analysis to be passed
+  # takes an expression matrix and a design matrix & returns a limma 
+  # fit
+  # 
+  # Args:
+  #   exprs: either a matrix, data.table, or EList of expression data
+  #   design.mat: a design matrix to be used for differential expression
+  #               analysis
+  # 
+  # Returns:
+  #   fit2: differential expression fit results, ready for use with 
+  #         limma::topTable
+  #         
+  # get data.table into correct format for use with limma 
+  if ("data.table" %in% class(exprs) & colnames(exprs)[1] == "gene") {
+    # error-handling 
+    if (!(all.equal(colnames(exprs)[2:ncol(exprs)], rownames(design.mat)))) {
+      stop("exprs colnames must match design.mat rownames")
+    } 
+    gene.ids <- exprs[["gene"]]
+    tmp.mat <- as.data.frame(exprs[, 2:ncol(exprs), with = FALSE])   
+    rownames(tmp.mat) <- gene.ids
+    tmp.mat <- t(apply(tmp.mat , 1, as.numeric))
+    colnames(tmp.mat) <- colnames(exprs)[2:ncol(exprs)]
+    exprs <- tmp.mat
+    rm(tmp.mat)
+  } else if (class(exprs) == "EList") {
+    # error-handling
+    if (!(all.equal(colnames(exprs$E), rownames(design.mat)))) {
+      stop("exprs colnames must match design.mat rownames")
+    } 
+  } else {
+    # error-handling 
+    if (!(all.equal(colnames(exprs), rownames(design.mat)))) {
+      stop("exprs colnames must match design.mat rownames")
+    } 
+  }
+  
+  #### differential expression ####
+  fit <- limma::lmFit(exprs, design.mat)
+  
+  # detect subtype to be used for differential expression
+  subtype <- colnames(design.mat)[which(colnames(design.mat) != "Other")]
+  if (subtype == "Her2") {
+    cont.eval <- "Her2vsOther= Her2-Other"
+  } else if (subtype == "LumA") {
+    cont.eval <- "LumAvsOther= LumA-Other"
+  } else if (subtype == "LumB") {
+    cont.eval <- "LumBvsOther= LumB-Other"
+  } else if (subtype == "Normal") {
+    cont.eval <- "NormalvsOther= Normal-Other"
+  } else {
+    cont.eval <- "BasalvsOther= Basal-Other"
+  }
+  
+  cont.matrix <-
+     eval(parse(
+       text = paste0(
+         'limma::makeContrasts(', cont.eval, ', levels = design.mat)')))
+  fit2 <- limma::contrasts.fit(fit, cont.matrix)
+  fit2 <- limma::eBayes(fit2)
+  return(fit2)
+  
+}  
 
 GetFiteBayesList <- function(norm.list, design.list) {
   # This function takes a nested list of normalized expression data and 
@@ -74,62 +141,6 @@ GetFiteBayesList <- function(norm.list, design.list) {
   #                    results) for each %seq and each normalization method
   #   
   
-  GetFiteBayes <- function(exprs, design.mat) {
-    # function for performing differential expression analysis to be passed
-    # to lapply
-    # get data.table into correct format for use with limma 
-    if ("data.table" %in% class(exprs) & colnames(exprs)[1] == "gene") {
-      # error-handling 
-      if (!(all.equal(colnames(exprs)[2:ncol(exprs)], rownames(design.mat)))) {
-        stop("exprs colnames must match design.mat rownames")
-      } 
-      gene.ids <- exprs[["gene"]]
-      tmp.mat <- as.data.frame(exprs[, 2:ncol(exprs), with = FALSE])   
-      rownames(tmp.mat) <- gene.ids
-      tmp.mat <- t(apply(tmp.mat , 1, as.numeric))
-      colnames(tmp.mat) <- colnames(exprs)[2:ncol(exprs)]
-      exprs <- tmp.mat
-      rm(tmp.mat)
-    } else if (class(exprs) == "EList") {
-      # error-handling
-      if (!(all.equal(colnames(exprs$E), rownames(design.mat)))) {
-        stop("exprs colnames must match design.mat rownames")
-      } 
-    } else {
-      # error-handling 
-      if (!(all.equal(colnames(exprs), rownames(design.mat)))) {
-        stop("exprs colnames must match design.mat rownames")
-      } 
-    }
-    
-    #### differential expression ####
-    fit <- limma::lmFit(exprs, design.mat)
-    
-    # detect subtype to be used for differential expression
-    subtype <- colnames(design.mat)[1]
-    if (subtype == "Her2") {
-      cont.matrix <- limma::makeContrasts(Her2vsOther= Her2-Other, 
-                                          levels = design.mat)
-    } else if (subtype == "LumA") {
-      cont.matrix <- limma::makeContrasts(LumAvsOther= LumA-Other, 
-                                          levels = design.mat)
-    } else if (subtype == "LumB") {
-      cont.matrix <- limma::makeContrasts(LumBvsOther= LumB-Other, 
-                                          levels = design.mat)
-    } else if (subtype == "Normal") {
-      cont.matrix <- limma::makeContrasts(NormalvsOther= Normal-Other, 
-                                          levels = design.mat)
-    } else {
-      cont.matrix <- limma::makeContrasts(BasalvsOther= Basal-Other, 
-                                          levels = design.mat)
-    }
-    
-    fit2 <- limma::contrasts.fit(fit, cont.matrix)
-    fit2 <- limma::eBayes(fit2)
-    return(fit2)
-  
-  }  
-  
   # want to assess the following normalization methods 
   norm.methods <- c("log", "npn", "qn", "tdm", "z", "qn-z", "un")
   
@@ -141,7 +152,7 @@ GetFiteBayesList <- function(norm.list, design.list) {
     # 100% RNA-seq data has special considerations wrt processing untransformed
     # count data (RSEM)
     if (seq.lvl == "100") {
-      nrm.mthds <- c("log", "npn", "qn", "tdm", "z", "qn-z")  # leave out counts
+      nrm.mthds <- head(norm.methods, -1)
       eBayes.fit.list[[seq.lvl]] <-
         lapply(norm.list[[seq.lvl]][names(norm.list[[seq.lvl]]) %in% 
                                       nrm.mthds],
