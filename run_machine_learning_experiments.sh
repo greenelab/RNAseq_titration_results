0d8b23aae3e030431de7503e29995718d7a20e8e
Steven Foltz <stevenmasonfoltz@gmail.com>
2021-06-21 09:50:23 -0400

    Run all the way through no exit 0

--- run_machine_learning_experiments.sh
+++ run_machine_learning_experiments.sh
@@ -3,8 +3,6 @@
 # Run ten repeats of the supervised analysis
 Rscript classifier_repeat_wrapper.R 1
 
-exit 0
-
 # Run the unsupervised analyses 
 Rscript 4-ica_pca_feature_reconstruction.R 50
 Rscript 5-predict_subtype_reconstructed_data.R
