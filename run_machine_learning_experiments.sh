f64ce11b7065705d6177595c5dc577a351b067b0
Steven Foltz <stevenmasonfoltz@gmail.com>
2021-06-29 16:52:15 -0400

    Run with 5- mod

--- run_machine_learning_experiments.sh
+++ run_machine_learning_experiments.sh
@@ -5,5 +5,5 @@ Rscript classifier_repeat_wrapper.R 1
 
 # Run the unsupervised analyses 
 Rscript 4-ica_pca_feature_reconstruction.R 50
-Rscript 5-predict_subtype_reconstructed_data.R
+Rscript 5-predict_subtype_reconstructed_data.mod.R
 Rscript 6-plot_recon_error_kappa.R
