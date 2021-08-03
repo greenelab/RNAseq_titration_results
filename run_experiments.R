9b4e6def4a04d5ebc4291cc48936c1b8759a48eb
Steven Foltz <stevenmasonfoltz@gmail.com>
2021-08-03 11:23:27 -0400

    Initial optparse for cancer type in setting up each repeat

--- run_experiments.R
+++ run_experiments.R
@@ -2,17 +2,30 @@
 # The purpose of this script is to run the BRCA subtype classifier pipeline
 # for RNA-seq 'titration.'
 # It should be run from the command line.
-# USAGE: Rscript run_experiments.R seed
-# where seed is an integer
+# USAGE: Rscript run_experiments.R --cancer_type [BRCA|GBM] --seed integer
 # It also may be run through the classifier_repeat_wrapper.R
 
-args <- commandArgs(trailingOnly = TRUE)
+option_list <- list(
+  optparse::make_option("--cancer_type",
+                        default = NULL,
+                        help = "Cancer type"),
+  optparse::make_option("--seed",
+                        default = NULL,
+                        help = "Random seed used to initiate this repeat")
+)
 
-initial.seed <- as.integer(args[1])
+opt <- optparse::parse_args(optparse::OptionParser(option_list=option_list))
+source("util/option_functions.R")
+check_options(opt)
+
+initial.seed <- opt$seed
 set.seed(initial.seed)
 
 # these seeds should be between 1000 and 9999 (be 4 digits) to match later file name parsing
 seeds <- sample(1000:9999, 3)
+
+message(paste("Initial seed:", initial.seed))
+message(paste("Secondary seeds:", stringr::str_c(seeds, collapse = ", ")))
 
 message("Getting overlap and splitting into training and testing sets...")
 system(paste("Rscript 0-expression_data_overlap_and_split.R", seeds[1]))
--- run_machine_learning_experiments.sh
+++ run_machine_learning_experiments.sh
@@ -1,9 +1,20 @@
 #!/bin/sh
+set -euo pipefail
+
+# cancer type (either BRCA or GBM)
+cancer_type=$1
+
+if [ $cancer_type = "BRCA" ] || [ $cancer_type = "GBM" ]; then
+  continue
+else
+  echo Cancer type must be BRCA or GBM in run_machine_learning_experiments.sh [cancer_type]
+  exit
+fi
 
 # Run ten repeats of the supervised analysis
-Rscript classifier_repeat_wrapper.R 10
+Rscript classifier_repeat_wrapper.R --cancer_type $cancer_type --n_repeats 10
 
 # Run the unsupervised analyses
-Rscript 4-ica_pca_feature_reconstruction.R 50
-Rscript 5-predict_subtype_reconstructed_data.R
-Rscript 6-plot_recon_error_kappa.R
+Rscript 4-ica_pca_feature_reconstruction.R --cancer_type $cancer_type --n_components 50
+Rscript 5-predict_subtype_reconstructed_data.R --cancer_type $cancer_type
+Rscript 6-plot_recon_error_kappa.R --cancer_type $cancer_type
