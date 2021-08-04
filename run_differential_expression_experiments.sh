a1630fa9b4522ca21a62b37a344831c0fbbd6784
Steven Foltz <stevenmasonfoltz@gmail.com>
2021-08-04 12:01:23 -0400

    Add pipefail and check for cancer type

--- run_differential_expression_experiments.sh
+++ run_differential_expression_experiments.sh
@@ -1,5 +1,17 @@
 #!/bin/sh
+set -euo pipefail
 
-Rscript 1A-detect_differentially_expressed_genes.R
-Rscript 2A-plot_DE_results.R
-Rscript 3A-small_n_differential_expression.R
+# cancer type (either BRCA or GBM)
+cancer_type=$1
+
+if [ $cancer_type = "BRCA" ] || [ $cancer_type = "GBM" ]; then
+  continue
+else
+  echo Cancer type must be BRCA or GBM in run_machine_learning_experiments.sh [cancer_type]
+  exit
+fi
+
+# Run differential expression scripts
+Rscript 1A-detect_differentially_expressed_genes.R --cancer_type $cancer_type
+Rscript 2A-plot_DE_results.R --cancer_type $cancer_type
+Rscript 3A-small_n_differential_expression.R --cancer_type $cancer_type
