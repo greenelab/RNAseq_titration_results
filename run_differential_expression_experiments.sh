6a1094674afcea98af00bcac54cd3d25967dac15
Steven Foltz <stevenmasonfoltz@gmail.com>
2021-08-04 15:17:08 -0400

    Allow different subtypes to be used for head to head comparison

--- run_differential_expression_experiments.sh
+++ run_differential_expression_experiments.sh
@@ -5,10 +5,12 @@ set -euo pipefail
 # where CANCER_TYPE is one of BRCA or GBM
 # SUBTYPE_VS_OTHER is the subtype you want to compare to all others (e.g. Basal)
 # SUBTYPE_VS_SUBTYPE is the two subtypes you want to compare head to head (e.g. LumA,Her2) (comma-separated)
+# SUBTYPE_VS_SUBTYPE_SMALL is the two subtypes you want to compare head to head when limiting the sample size (e.g. LumA,Her2) (comma-separated)
 
 cancer_type=$1
 subtype_vs_others=$2
 subtype_vs_subtype=$3
+subtype_vs_subytpe_small=$4
 
 if [ $cancer_type = "BRCA" ] || [ $cancer_type = "GBM" ]; then
   continue
@@ -20,4 +22,4 @@ fi
 # Run differential expression scripts
 Rscript 1A-detect_differentially_expressed_genes.R --cancer_type $cancer_type --subtype_vs_others $2 --subtype_vs_subtype $3
 Rscript 2A-plot_DE_results.R --cancer_type $cancer_type --subtype_vs_others $2 --subtype_vs_subtype $3
-Rscript 3A-small_n_differential_expression.R --cancer_type $cancer_type --subtype_vs_subtype $3
+Rscript 3A-small_n_differential_expression.R --cancer_type $cancer_type --subtype_vs_subtype $4
