9eee820dd52b3d74d6ad8f433073d86b4bcdecee
Steven Foltz <stevenmasonfoltz@gmail.com>
2021-08-20 14:24:49 -0400

    Install and run titration docker image from aws instance

---
+++ run_docker.sh
@@ -0,0 +1,13 @@
+# script to get docker image up and running
+
+sudo snap install docker
+
+sleep 3 && sudo chmod 666 /var/run/docker.sock
+
+docker pull envest/rnaseq_titration_results
+
+docker run -d --rm --mount type=volume,dst=/home/rstudio,volume-driver=local,volume-opt=type=none,volume-opt=o=bind,volume-opt=device=/home/ubuntu -e PASSWORD=onecupatatime envest/rnaseq_titration_results
+
+container_id=$(docker ps -ql)
+
+docker exec -it $container_id bash
