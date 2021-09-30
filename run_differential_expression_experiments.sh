b6e73f66a7821d990d0e6518d97f6c72d0e12731
Steven Foltz <stevenmasonfoltz@gmail.com>
2021-09-30 15:34:06 -0400

    Update shell script with words

--- run_differential_expression_experiments.sh
+++ run_differential_expression_experiments.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -euo pipefail
 
-# Usage: bash run_differential_expression_experiments.sh CANCER_TYPE SUBTYPE_VS_OTHERS SUBTYPE_VS_SUBTYPE
+# Usage: bash run_differential_expression_experiments.sh CANCER_TYPE SUBTYPE_VS_OTHERS SUBTYPE_VS_SUBTYPE SUBTYPE_VS_SUBTYPE_SMALL
 # where CANCER_TYPE is one of BRCA or GBM
 # SUBTYPE_VS_OTHER is the subtype you want to compare to all others (e.g. Basal)
 # SUBTYPE_VS_SUBTYPE is the two subtypes you want to compare head to head (e.g. LumA,Her2) (comma-separated)
@@ -13,11 +13,11 @@ subtype_vs_subtype=$3
 subtype_vs_subytpe_small=$4
 
 if [ $cancer_type != "BRCA" ] && [ $cancer_type != "GBM" ]; then
-  echo Cancer type must be BRCA or GBM in run_machine_learning_experiments.sh [cancer_type]
+  echo Cancer type must be BRCA or GBM in run_differential_expression_experiments.sh [cancer_type]
   exit
 fi
 
 # Run differential expression scripts
-Rscript 1A-detect_differentially_expressed_genes.R --cancer_type $cancer_type --subtype_vs_others $2 --subtype_vs_subtype $3
-Rscript 2A-plot_DE_results.R --cancer_type $cancer_type --subtype_vs_others $2 --subtype_vs_subtype $3
-Rscript 3A-small_n_differential_expression.R --cancer_type $cancer_type --subtype_vs_subtype $4
+Rscript 1A-detect_differentially_expressed_genes.R --cancer_type $cancer_type --subtype_vs_others $subtype_vs_others --subtype_vs_subtype $subtype_vs_subtype
+Rscript 2A-plot_DE_results.R --cancer_type $cancer_type --subtype_vs_others $subtype_vs_others --subtype_vs_subtype $subtype_vs_subtype
+Rscript 3A-small_n_differential_expression.R --cancer_type $cancer_type --subtype_vs_subtype $subtype_vs_subtype_small
