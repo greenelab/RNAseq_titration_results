052725af370ec215d2ea69394241bf5639bd820b
Steven Foltz <stevenmasonfoltz@gmail.com>
2021-12-17 12:07:21 -0500

    Undo change to Treat untransformed RNA-seq explicitly as data.matrix

--- util/normalization_functions.R
+++ util/normalization_functions.R
@@ -342,7 +342,7 @@ SinglePlatformNormalizationWrapper <- function(dt, platform = "array",
       # by design, untransformed data should not be zero to one transformed,
       # so just add the data.table (dt) that contains RNA-seq data (RSEM)
       # to the list
-      norm.list[["un"]] <- data.matrix(dt)
+      norm.list[["un"]] <- dt
     }
   } else {
     stop("platform parameter should be set to 'array' or 'seq'")
