8e2985f173b3b965e13d2b8b423afb9efddec5b0
Steven Foltz <stevenmasonfoltz@gmail.com>
2022-01-07 11:42:45 -0500

    use new error check function name

--- 7-extract_plier_pathways.R
+++ 7-extract_plier_pathways.R
@@ -322,7 +322,7 @@ for (seed_index in 1:length(norm.train.files)) {
   # Check for failure to converge, and set to NA
   
   plier_results_list <- purrr::modify_depth(plier_results_list, 2,
-                                            check_plier_failure
+                                            check_plier_failure_to_converge
   )
   
   # Return pathway comparison for appropriate level of PLIER results list
