e61bbdc21027a02bdbe36dad8effa43c5b70b8ab
Steven Foltz <stevenmasonfoltz@gmail.com>
2022-01-12 11:30:38 -0500

    Plot jaccard df

--- 7-extract_plier_pathways.R
+++ 7-extract_plier_pathways.R
@@ -38,6 +38,7 @@ message(paste("\nPLIER initial seed set to:", initial.seed))
 data.dir <- here::here("data")
 norm.data.dir <- here::here("normalized_data")
 res.dir <- here::here("results")
+plots.dir <- here::here("plots")
 
 # define input files
 norm.train.files <- file.path(
@@ -339,5 +340,34 @@ if (length(jaccard_list) > 0) {
     )
   )
   
-  # TODO PLOT THAT
+  # Plot results
+  
+  plot_filename = file.path(
+    plots.dir,
+    str_c(file_identifier, "_PLIER_jaccard.pdf")
+  )
+  
+  jaccard_df %>%
+    mutate(pseq = as.factor(pseq),
+           nmeth = str_to_upper(nmeth)) %>%
+    ggplot(aes(x = pseq,
+               y = jaccard)) +
+    geom_violin() +
+    stat_summary(fun = median, geom = "line", aes(group = "pseq"),
+                 position = position_dodge(0.6)) +
+    stat_summary(fun = median, geom = "point", aes(group = "pseq"),
+                 position = position_dodge(0.7), size = 1) +
+    expand_limits(y = 0) +
+    facet_wrap(~ nmeth,
+               nrow = 1) +
+    ggtitle(cancer_type) +
+    xlab("% RNA-seq samples") +
+    ylab("Proportion") +
+    theme_bw() +
+    theme(text = element_text(size = 18)) +
+    theme(axis.text.x = element_text(angle = 45, vjust = 0.5))
+  
+  ggsave(plot_filename,
+         plot = last_plot(),
+         height = 3.5, width = 15)
 }
