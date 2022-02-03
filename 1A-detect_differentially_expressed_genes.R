80f50746c9c71a2ac1057bb55d4a661007aece70
Steven Foltz <stevenmasonfoltz@gmail.com>
2022-02-03 09:24:17 -0500

    Define plot.data.dir

--- 1A-detect_differentially_expressed_genes.R
+++ 1A-detect_differentially_expressed_genes.R
@@ -49,6 +49,7 @@ data.dir <- here::here("data")
 res.dir <- here::here("results")
 norm.dir <- here::here("normalized_data")
 deg.dir <- file.path(res.dir, "differential_expression")
+plot.data.dir <- here::here("plots/data")
 
 # define input files
 seq.file <- file.path(data.dir,
