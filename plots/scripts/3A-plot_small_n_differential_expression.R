78aa77bf9ac7d8992000de7e16191d2c9e30b11d
Steven Foltz <stevenmasonfoltz@gmail.com>
2022-02-28 10:47:35 -0500

    Allow output directory and measures to be specified

--- plots/scripts/3A-plot_small_n_differential_expression.R
+++ plots/scripts/3A-plot_small_n_differential_expression.R
@@ -1,24 +1,57 @@
 # J. Taroni Feb 2016, S. Foltz Feb 2022
 # With small n data, plot comparison of SOMETHING
 #
-# USAGE: Rscript 3A-plot_small_n_differential_expression.R --cancer_type --subtype_vs_subtype --supplementary
+# USAGE: Rscript 3A-plot_small_n_differential_expression.R --cancer_type --subtype_vs_others --subtype_vs_subtype --output_directory --overlap_measure
 
 option_list <- list(
   optparse::make_option("--cancer_type",
                         default = NA_character_,
                         help = "Cancer type"),
+  optparse::make_option("--subtype_vs_others",
+                        help = "Subtype used for comparison against all others."),
   optparse::make_option("--subtype_vs_subtype",
-                        default = NA_character_,
                         help = "Subtypes used in head-to-head comparison (comma-separated without space e.g. Type1,Type2)"),
-  optparse::make_option("--supplementary",
-                        action = "store_true",
-                        default = FALSE,
-                        help = "Save plot in plots/supplementary folder instead of plots/main")
+  optparse::make_option("--output_directory",
+                        default = NA_character_,
+                        help = "Output directory of DEG overlap plot (absolute or relative path)."),
+  optparse::make_option("--overlap_measure",
+                        default = NA_character_,
+                        help = "Which overlap measures to include in silver standard overlap plot (comma-separated without space e.g. Jaccard,Rand,Spearman; must be one or more of Jaccard, Rand, Spearman)")
 )
 
 opt <- optparse::parse_args(optparse::OptionParser(option_list=option_list))
 source(here::here("util/option_functions.R"))
 check_options(opt)
+
+# at least one of --subtype_vs_others or --subtype_vs_subtype should be given
+if (any(c("subtype_vs_others", "subtype_vs_subtype") %in% names(opt))) {
+  
+  subtype_vs_others <- NA # first assume option is not provided
+  subtype_vs_subtype <- NA # then update as available below
+  
+  if ("subtype_vs_others" %in% names(opt)) {
+    subtype_vs_others <- opt$subtype_vs_others
+  }
+  
+  if ("subtype_vs_subtype" %in% names(opt)) {
+    subtype_vs_subtype <- opt$subtype_vs_subtype
+  }
+  
+} else {
+  message("  Errors: must include --subtype_vs_others and/or --subtype_vs_subtype in plots/scripts/3A-plot_small_n_differential_expression.R.\n")
+  stop()
+}
+
+# check that overlap measures requested are the ones present in data
+if ("overlap_measure" %in% names(opt)) {
+  overlap_measures <- sort(stringr::str_split(opt$overlap_measure,
+                                              pattern = ",", simplify = TRUE))
+  
+  if (!all(overlap_measures %in% c("Jaccard", "Rand", "Spearman"))) {
+    message("  Errors: --overlap_measure must be one or more of Jaccard, Rand, Spearman in plots/scripts/3A-plot_small_n_differential_expression.R.\n")
+    stop()  
+  }
+}
 
 # load libraries
 suppressMessages(library(tidyverse))
@@ -26,20 +59,12 @@ source(here::here("util/color_blind_friendly_palette.R"))
 
 # set options
 cancer_type <- opt$cancer_type
-subtype_vs_subtype <- opt$subtype_vs_subtype
-supplementary <- opt$supplementary
-two_subtypes <- as.vector(stringr::str_split(subtype_vs_subtype, pattern = ",", simplify = TRUE))
 file_identifier <- str_c(cancer_type, "subtype", sep = "_") # we are only working with subtype models here
 
 # define directories
 plot.dir <- here::here("plots")
-plot.main.dir <- file.path(plot.dir, "main")
-plot.supp.dir <- file.path(plot.dir, "supplementary")
 plot.data.dir <- file.path(plot.dir, "data")
-
-output_directory <- file.path(ifelse(supplementary,
-                                     plot.supp.dir,
-                                     plot.main.dir))
+output_directory <- opt$output_directory
 
 #### functions -----------------------------------------------------------------
 
@@ -55,14 +80,48 @@ DataSummary <- function(x) {
   return(c(y = m, ymin = ymin, ymax = ymax))
 }
 
-plot_small_n <- function(df, single_measure = FALSE){
+plot_small_n <- function(subtypes){
   # This function creates a panel of line plots faceted by 
   # % RNA-seq, the measure (Jaccard, Rand, Spearman), and normalization method
-  # If single measure == TRUE, then only one measure should be present in df
   
-  plot_obj <- ggplot(df, aes(x = no.samples,
-                             y = value,
-                             color = platform)) +
+  subtypes_path <- str_c(subtypes, collapse = "v")
+  subtypes_nice <- str_c(subtypes, collapse = " vs. ")
+  measures_path <- str_c(overlap_measures, collapse = "_")
+  
+  input_filename <- file.path(plot.data.dir,
+                              paste0(file_identifier,
+                                     "_small_n_",
+                                     subtypes_path,
+                                     "_results.tsv"))
+  
+  output_filename <- file.path(
+    output_directory,
+    paste0(file_identifier, "_small_n_",
+           measures_path, "_", subtypes_path, ".pdf"))
+  
+  using_single_measure <- length(overlap_measures) == 1
+  
+  #stats_df <- read_tsv(input_filename,
+  #                     col_types = "ccdddddd") %>%
+  #  filter(seq_prop %in% str_c(c(30, 50, 70), "% RNA-seq"),
+  #         !is.na(value),
+  #         measure %in% overlap_measures) %>%
+  #  mutate(no.samples = factor(no.samples))
+
+  stats_df <- read_tsv(input_filename,
+                       col_types = "ccdddddd") %>%
+    rename("Jaccard" = "jaccard", "Rand" = "rand", "Spearman" = "spearman") %>%
+    gather(key = "measure", value = "value", Jaccard, Rand, Spearman) %>%
+    mutate(seq_prop = factor(str_c(seq_prop, "% RNA-seq"),
+                             levels = str_c(seq(0, 100, 10), "% RNA-seq"))) %>%
+    filter(seq_prop %in% str_c(c(30, 50, 70), "% RNA-seq"),
+           !is.na(value),
+           measure %in% overlap_measures) %>%
+    mutate(no.samples = factor(no.samples))
+  
+  plot_obj <- ggplot(stats_df, aes(x = no.samples,
+                                   y = value,
+                                   color = platform)) +
     stat_summary(fun = median,
                  geom = "line",
                  aes(group = platform),
@@ -82,13 +141,13 @@ plot_small_n <- function(df, single_measure = FALSE){
     scale_y_continuous(breaks = seq(0, 1, 0.25)) +
     theme_bw() +
     labs(x = "Number of samples",
-         y = ifelse(single_measure,
-                    unique(df$measure),
+         y = ifelse(using_single_measure,
+                    unique(stats_df$measure),
                     "Measure of similarity"),
-         title = paste(cancer_type, subtypes_combination_nice, "FDR < 10%")) +
+         title = paste(cancer_type, subtypes_nice, "FDR < 10%")) +
     scale_colour_manual(values = cbPalette[c(2, 3)])
   
-  if (single_measure) {
+  if (using_single_measure) {
     plot_obj <- plot_obj +
       facet_grid(normalization ~ seq_prop)
   } else {
@@ -96,37 +155,25 @@ plot_small_n <- function(df, single_measure = FALSE){
       facet_grid(measure + normalization ~ seq_prop)
   }
   
-  return(plot_obj)
+  ggsave(filename = output_filename,
+         plot = plot_obj,
+         width = 7.25,
+         height = c(3,4,5)[length(overlap_measures)])
+}
+
+#### plot Subtype v. Other results ---------------------------------------------
+
+if (!is.na(subtype_vs_others)) {
+  
+  subtypes <- c(subtype_vs_others, "Other")
+  plot_small_n(subtypes)
 }
 
 #### plot Subtype v. Subtype results -------------------------------------------
-subtypes_combination <- stringr::str_c(two_subtypes, collapse = "v")
-subtypes_combination_nice <- stringr::str_c(two_subtypes, collapse = " vs. ")
 
-stats_df <- read_tsv(file.path(plot.data.dir,
-                               paste0(file_identifier,
-                                      "_small_n_",
-                                      subtypes_combination,
-                                      "_results.tsv")),
-                     col_types = "ccdddddd") %>%
-  filter(seq_prop %in% str_c(c(30, 50, 70), "% RNA-seq"),
-         !is.na(value)) %>%
-  mutate(no.samples = factor(no.samples))
-
-# plot with plot_small_n() and save files
-
-ggsave(filename = here::here(output_directory,
-                             paste0(file_identifier, "_small_n_",
-                                    subtypes_combination, ".pdf")),
-       plot = plot_small_n(stats_df),
-       width = 7.25,
-       height = 6)
-
-ggsave(filename = here::here(output_directory,
-                             paste0(file_identifier, "_small_n_",
-                                    subtypes_combination, ".jaccard_only.pdf")),
-       plot = plot_small_n(stats_df %>%
-                             filter(measure == "Jaccard"),
-                           single_measure = TRUE),
-       width = 7.25,
-       height = 3)
+if (!is.na(subtype_vs_subtype)) {
+  
+  subtypes <- as.vector(
+    stringr::str_split(subtype_vs_subtype, pattern = ",", simplify = TRUE))
+  plot_small_n(subtypes)
+}
