5faafd744e4874a0a8ed55ed4d5dbf37ade06a43
Steven Foltz <stevenmasonfoltz@gmail.com>
2022-03-02 11:01:30 -0500

    Add all predictors and cancer types

--- run_everything.sh
+++ run_everything.sh
@@ -1,5 +1,125 @@
 #!/bin/sh
 
+# This script runs all analysis code and plotting scripts for the publication,
+# including BRCA (subtype, TP53, PIK3CA) and GBM (subtype, TP53, PIK3CA).
+# The script calls:
+#  1. run_machine_learning_experiments.sh
+#  2. run_differential_expression_experiments.sh (subtype only)
+#  3. visualize_expression.R (subtype only)
+#  4. plotting scripts, as appropriate
+
+################################################################################
+# BRCA
+################################################################################
+
+# BRCA subtype -----------------------------------------------------------------
+
+bash run_machine_learning_experiments.sh BRCA subtype 7
+bash run_differential_expression_experiments.sh BRCA Basal Her2,LumA Her2,LumA 7
+
+Rscript visualize_expression.R --cancer_type BRCA --predictor subtype
+
+Rscript plots/scripts/0-plot_predictor_category_distributions.R \
+  --cancer_type BRCA \
+  --predictor subtype \
+  --output_directory plots/supplementary
+
+Rscript plots/scripts/0-plot_predictor_category_distributions.R \
+  --cancer_type BRCA \
+  --predictor subtype \
+  --plot_all_seeds \
+  --output_directory plots/supplementary
+
+Rscript plots/scripts/3-plot_category_kappa.R \
+  --cancer_type BRCA \
+  --predictor subtype \
+  --output_directory plots/main
+
+Rscript plots/scripts/6-plot_recon_kappa.R \
+  --cancer_type BRCA \
+  --predictor subtype \
+  --output_directory plots/supplementary
+
+Rscript plots/scripts/6-plot_recon_error.R \
+  --cancer_type BRCA \
+  --predictor subtype \
+  --output_directory plots/supplementary
+
+Rscript plots/scripts/7-plot_plier_pathways.R \
+  --cancer_type BRCA \
+  --predictor subtype \
+  --output_directory plots/main
+
+Rscript plots/scripts/2A-plot_DEGs.R \
+  --cancer_type BRCA \
+  --subtype_vs_others Basal \
+  --proportion_output_directory plots/supplementary \
+  --overlap_output_directory plots/supplementary \
+  --overlap_measure Jaccard
+  
+Rscript plots/scripts/2A-plot_DEGs.R \
+  --cancer_type BRCA \
+  --subtype_vs_subtype Her2,LumA \
+  --proportion_output_directory plots/supplementary \
+  --overlap_output_directory plots/main \
+  --overlap_measure Jaccard
+
+Rscript plots/scripts/3A-plot_small_n_differential_expression.R \
+  --cancer_type BRCA \
+  --subtype_vs_subtype Her2,LumA \
+  --output_directory plots/main \
+  --overlap_measure Jaccard,Spearman
+
+# ------------------------------------------------------------------------------
+
+# BRCA TP53 --------------------------------------------------------------------
+
+bash run_machine_learning_experiments.sh BRCA TP53 7
+
+Rscript plots/scripts/0-plot_predictor_category_distributions.R \
+  --cancer_type BRCA \
+  --predictor TP53 \
+  --output_directory plots/supplementary
+
+Rscript plots/scripts/0-plot_predictor_category_distributions.R \
+  --cancer_type BRCA \
+  --predictor TP53 \
+  --plot_all_seeds \
+  --output_directory plots/supplementary
+
+Rscript plots/scripts/3-plot_category_kappa.R \
+  --cancer_type BRCA \
+  --predictor TP53 \
+  --output_directory plots/supplementary
+
+# ------------------------------------------------------------------------------
+
+# BRCA PIK3CA ------------------------------------------------------------------
+
+bash run_machine_learning_experiments.sh BRCA PIK3CA 7
+
+Rscript plots/scripts/0-plot_predictor_category_distributions.R \
+  --cancer_type BRCA \
+  --predictor PIK3CA \
+  --output_directory plots/supplementary
+
+Rscript plots/scripts/0-plot_predictor_category_distributions.R \
+  --cancer_type BRCA \
+  --predictor PIK3CA \
+  --plot_all_seeds \
+  --output_directory plots/supplementary
+
+Rscript plots/scripts/3-plot_category_kappa.R \
+  --cancer_type BRCA \
+  --predictor PIK3CA \
+  --output_directory plots/supplementary
+
+# ------------------------------------------------------------------------------
+
+################################################################################
+# GBM
+################################################################################
+
 # GBM subtype ------------------------------------------------------------------
 
 bash run_machine_learning_experiments.sh GBM subtype 7
@@ -51,5 +171,49 @@ Rscript plots/scripts/3A-plot_small_n_differential_expression.R \
   --subtype_vs_subtype Classical,Mesenchymal \
   --output_directory plots/supplementary \
   --overlap_measure Jaccard,Spearman
+
+# ------------------------------------------------------------------------------
+
+# GBM TP53 ---------------------------------------------------------------------
+
+bash run_machine_learning_experiments.sh GBM TP53 7
+
+Rscript plots/scripts/0-plot_predictor_category_distributions.R \
+  --cancer_type GBM \
+  --predictor TP53 \
+  --output_directory plots/supplementary
+
+Rscript plots/scripts/0-plot_predictor_category_distributions.R \
+  --cancer_type GBM \
+  --predictor TP53 \
+  --plot_all_seeds \
+  --output_directory plots/supplementary
+
+Rscript plots/scripts/3-plot_category_kappa.R \
+  --cancer_type GBM \
+  --predictor TP53 \
+  --output_directory plots/main
+
+# ------------------------------------------------------------------------------
+
+# GBM PIK3CA -------------------------------------------------------------------
+
+bash run_machine_learning_experiments.sh GBM PIK3CA 7
+
+Rscript plots/scripts/0-plot_predictor_category_distributions.R \
+  --cancer_type GBM \
+  --predictor PIK3CA \
+  --output_directory plots/supplementary
+
+Rscript plots/scripts/0-plot_predictor_category_distributions.R \
+  --cancer_type GBM \
+  --predictor PIK3CA \
+  --plot_all_seeds \
+  --output_directory plots/supplementary
+
+Rscript plots/scripts/3-plot_category_kappa.R \
+  --cancer_type GBM \
+  --predictor PIK3CA \
+  --output_directory plots/supplementary
 
 # ------------------------------------------------------------------------------
