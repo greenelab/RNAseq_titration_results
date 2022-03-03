1f66f83c21db25810ecdf2cdd03490dd73cee87d
Steven Foltz <stevenmasonfoltz@gmail.com>
2022-03-03 12:40:40 -0500

    Fix path to visualize_expression.R and add comments

--- run_all_analyses_and_plots.sh
+++ run_all_analyses_and_plots.sh
@@ -5,7 +5,7 @@
 # The script calls:
 #  1. run_machine_learning_experiments.sh
 #  2. run_differential_expression_experiments.sh (subtype only)
-#  3. visualize_expression.R (subtype only)
+#  3. plots/scripts/visualize_expression.R (subtype only)
 #  4. plotting scripts, as appropriate
 
 ################################################################################
@@ -14,42 +14,52 @@
 
 # BRCA subtype -----------------------------------------------------------------
 
+# run machine learning and DEG analysis scripts
 bash run_machine_learning_experiments.sh BRCA subtype 7
 bash run_differential_expression_experiments.sh BRCA Basal Her2,LumA Her2,LumA 7
 
-Rscript visualize_expression.R --cancer_type BRCA --predictor subtype
+# plot array vs. RNA-seq expression levels after normalization
+Rscript plots/scripts/visualize_expression.R --cancer_type BRCA --predictor subtype
 
+# stacked bar plot showing distribution of subtypes in train/test sets (one representative example)
 Rscript plots/scripts/0-plot_predictor_category_distributions.R \
   --cancer_type BRCA \
   --predictor subtype \
   --output_directory plots/supplementary
 
+# stacked bar plots showing distribution of subtypes in train/test sets (all seeds)
 Rscript plots/scripts/0-plot_predictor_category_distributions.R \
   --cancer_type BRCA \
   --predictor subtype \
   --plot_all_seeds \
   --output_directory plots/supplementary
 
+# violin + line plots showing kappa values from predictions on test data
 Rscript plots/scripts/3-plot_category_kappa.R \
   --cancer_type BRCA \
   --predictor subtype \
   --output_directory plots/main
 
+# violin + line plots showing kappa values from predictions on reconstructed test data
 Rscript plots/scripts/6-plot_recon_kappa.R \
   --cancer_type BRCA \
   --predictor subtype \
   --output_directory plots/supplementary
 
+# violin + line plots showing gene-level MASE values from reconstructed test data
 Rscript plots/scripts/6-plot_recon_error.R \
   --cancer_type BRCA \
   --predictor subtype \
   --output_directory plots/supplementary
 
+# violin plots showing proportion of pathways significant in PLIER analyses
 Rscript plots/scripts/7-plot_plier_pathways.R \
   --cancer_type BRCA \
   --predictor subtype \
   --output_directory plots/main
 
+# bar plot showing proportion of genes differentially expressed (Basal vs. Others)
+# line plot showing overlap with silver standard DEGs (Basal vs. Others)
 Rscript plots/scripts/2A-plot_DEGs.R \
   --cancer_type BRCA \
   --subtype_vs_others Basal \
@@ -57,6 +67,8 @@ Rscript plots/scripts/2A-plot_DEGs.R \
   --overlap_output_directory plots/supplementary \
   --overlap_measure Jaccard
   
+# bar plot showing proportion of genes differentially expressed (Her2 vs. LumA)
+# line plot showing overlap with silver standard DEGs (Her2 vs. LumA)
 Rscript plots/scripts/2A-plot_DEGs.R \
   --cancer_type BRCA \
   --subtype_vs_subtype Her2,LumA \
@@ -64,6 +76,7 @@ Rscript plots/scripts/2A-plot_DEGs.R \
   --overlap_output_directory plots/main \
   --overlap_measure Jaccard
 
+# line plot showing overlap with silver standard DEGs (Her2 vs. LumA) across small n values
 Rscript plots/scripts/3A-plot_small_n_differential_expression.R \
   --cancer_type BRCA \
   --subtype_vs_subtype Her2,LumA \
@@ -74,19 +87,23 @@ Rscript plots/scripts/3A-plot_small_n_differential_expression.R \
 
 # BRCA TP53 --------------------------------------------------------------------
 
+# run machine learning analysis scripts
 bash run_machine_learning_experiments.sh BRCA TP53 7
 
+# stacked bar plot showing distribution of subtypes in train/test sets (one representative example)
 Rscript plots/scripts/0-plot_predictor_category_distributions.R \
   --cancer_type BRCA \
   --predictor TP53 \
   --output_directory plots/supplementary
 
+# stacked bar plots showing distribution of subtypes in train/test sets (all seeds)
 Rscript plots/scripts/0-plot_predictor_category_distributions.R \
   --cancer_type BRCA \
   --predictor TP53 \
   --plot_all_seeds \
   --output_directory plots/supplementary
 
+# violin + line plots showing kappa values from predictions on test data
 Rscript plots/scripts/3-plot_category_kappa.R \
   --cancer_type BRCA \
   --predictor TP53 \
@@ -96,19 +113,23 @@ Rscript plots/scripts/3-plot_category_kappa.R \
 
 # BRCA PIK3CA ------------------------------------------------------------------
 
+# run machine learning analysis scripts
 bash run_machine_learning_experiments.sh BRCA PIK3CA 7
 
+# stacked bar plot showing distribution of subtypes in train/test sets (one representative example)
 Rscript plots/scripts/0-plot_predictor_category_distributions.R \
   --cancer_type BRCA \
   --predictor PIK3CA \
   --output_directory plots/supplementary
 
+# stacked bar plots showing distribution of subtypes in train/test sets (all seeds)
 Rscript plots/scripts/0-plot_predictor_category_distributions.R \
   --cancer_type BRCA \
   --predictor PIK3CA \
   --plot_all_seeds \
   --output_directory plots/supplementary
 
+# violin + line plots showing kappa values from predictions on test data
 Rscript plots/scripts/3-plot_category_kappa.R \
   --cancer_type BRCA \
   --predictor PIK3CA \
@@ -122,42 +143,52 @@ Rscript plots/scripts/3-plot_category_kappa.R \
 
 # GBM subtype ------------------------------------------------------------------
 
+# run machine learning and DEG analysis scripts
 bash run_machine_learning_experiments.sh GBM subtype 7
 bash run_differential_expression_experiments.sh GBM Proneural Classical,Mesenchymal Classical,Mesenchymal 7
 
-Rscript visualize_expression.R --cancer_type GBM --predictor subtype
+# plot array vs. RNA-seq expression levels after normalization
+Rscript plots/scripts/visualize_expression.R --cancer_type GBM --predictor subtype
 
+# stacked bar plot showing distribution of subtypes in train/test sets (one representative example)
 Rscript plots/scripts/0-plot_predictor_category_distributions.R \
   --cancer_type GBM \
   --predictor subtype \
   --output_directory plots/supplementary
 
+# stacked bar plots showing distribution of subtypes in train/test sets (all seeds)
 Rscript plots/scripts/0-plot_predictor_category_distributions.R \
   --cancer_type GBM \
   --predictor subtype \
   --plot_all_seeds \
   --output_directory plots/supplementary
 
+# violin + line plots showing kappa values from predictions on test data
 Rscript plots/scripts/3-plot_category_kappa.R \
   --cancer_type GBM \
   --predictor subtype \
   --output_directory plots/supplementary
 
+# violin + line plots showing kappa values from predictions on reconstructed test data
 Rscript plots/scripts/6-plot_recon_kappa.R \
   --cancer_type GBM \
   --predictor subtype \
   --output_directory plots/supplementary
 
+# violin + line plots showing gene-level MASE values from reconstructed test data
 Rscript plots/scripts/6-plot_recon_error.R \
   --cancer_type GBM \
   --predictor subtype \
   --output_directory plots/supplementary
 
+# violin plots showing proportion of pathways significant in PLIER analyses
 Rscript plots/scripts/7-plot_plier_pathways.R \
   --cancer_type GBM \
   --predictor subtype \
   --output_directory plots/main
 
+# bar plot showing proportion of genes differentially expressed (Proneural vs. Others, Classical vs. Mesenchymal)
+# line plot showing overlap with silver standard DEGs (Proneural vs. Others, Classical vs. Mesenchymal)
 Rscript plots/scripts/2A-plot_DEGs.R \
   --cancer_type GBM \
   --subtype_vs_others Proneural \
@@ -166,6 +197,7 @@ Rscript plots/scripts/2A-plot_DEGs.R \
   --overlap_output_directory plots/supplementary \
   --overlap_measure Jaccard
 
+# line plot showing overlap with silver standard DEGs (Classical vs. Mesenchymal) across small n values
 Rscript plots/scripts/3A-plot_small_n_differential_expression.R \
   --cancer_type GBM \
   --subtype_vs_subtype Classical,Mesenchymal \
@@ -176,19 +208,23 @@ Rscript plots/scripts/3A-plot_small_n_differential_expression.R \
 
 # GBM TP53 ---------------------------------------------------------------------
 
+# run machine learning analysis scripts
 bash run_machine_learning_experiments.sh GBM TP53 7
 
+# stacked bar plot showing distribution of subtypes in train/test sets (one representative example)
 Rscript plots/scripts/0-plot_predictor_category_distributions.R \
   --cancer_type GBM \
   --predictor TP53 \
   --output_directory plots/supplementary
 
+# stacked bar plots showing distribution of subtypes in train/test sets (all seeds)
 Rscript plots/scripts/0-plot_predictor_category_distributions.R \
   --cancer_type GBM \
   --predictor TP53 \
   --plot_all_seeds \
   --output_directory plots/supplementary
 
+# violin + line plots showing kappa values from predictions on test data
 Rscript plots/scripts/3-plot_category_kappa.R \
   --cancer_type GBM \
   --predictor TP53 \
@@ -198,19 +234,23 @@ Rscript plots/scripts/3-plot_category_kappa.R \
 
 # GBM PIK3CA -------------------------------------------------------------------
 
+# run machine learning analysis scripts
 bash run_machine_learning_experiments.sh GBM PIK3CA 7
 
+# stacked bar plot showing distribution of subtypes in train/test sets (one representative example)
 Rscript plots/scripts/0-plot_predictor_category_distributions.R \
   --cancer_type GBM \
   --predictor PIK3CA \
   --output_directory plots/supplementary
 
+# stacked bar plots showing distribution of subtypes in train/test sets (all seeds)
 Rscript plots/scripts/0-plot_predictor_category_distributions.R \
   --cancer_type GBM \
   --predictor PIK3CA \
   --plot_all_seeds \
   --output_directory plots/supplementary
 
+# violin + line plots showing kappa values from predictions on test data
 Rscript plots/scripts/3-plot_category_kappa.R \
   --cancer_type GBM \
   --predictor PIK3CA \
