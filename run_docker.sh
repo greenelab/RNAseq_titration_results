bb3a581df26b4a2947f6424694af36d63309b38c
Steven Foltz <stevenmasonfoltz@gmail.com>
2022-04-01 09:46:55 -0400

    Specify R-4.1.2 tag for docker image pulled

--- run_docker.sh
+++ run_docker.sh
@@ -4,9 +4,9 @@ sudo snap install docker
 
 sleep 3 && sudo chmod 666 /var/run/docker.sock
 
-docker pull envest/rnaseq_titration_results
+docker pull envest/rnaseq_titration_results:R-4.1.2
 
-docker run -d --rm --mount type=volume,dst=/home/rstudio,volume-driver=local,volume-opt=type=none,volume-opt=o=bind,volume-opt=device=/home/ubuntu -e PASSWORD=onecupatatime envest/rnaseq_titration_results
+docker run -d --rm --mount type=volume,dst=/home/rstudio,volume-driver=local,volume-opt=type=none,volume-opt=o=bind,volume-opt=device=/home/ubuntu -e PASSWORD=onecupatatime envest/rnaseq_titration_results:R-4.1.2
 
 container_id=$(docker ps -ql)
 
