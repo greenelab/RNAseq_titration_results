375f0f01fa6a547869a7a6c4042b33daa13fd2b8
Steven Foltz <stevenmasonfoltz@gmail.com>
2022-07-15 15:13:30 -0400

    Remove comments inserted during testing

--- run_machine_learning_experiments.sh
+++ run_machine_learning_experiments.sh
@@ -22,15 +22,14 @@ if [ $predictor == "TP53" ] || [ $predictor == "PIK3CA" ]; then
   Rscript classifier_repeat_wrapper.R --cancer_type $cancer_type --predictor $predictor --n_repeats 10 --ncores $ncores
   Rscript classifier_repeat_wrapper.R --cancer_type $cancer_type --predictor $predictor --n_repeats 10 --null_model --ncores $ncores
 else
-  #Rscript classifier_repeat_wrapper.R --cancer_type $cancer_type --predictor $predictor --n_repeats 10 --ncores $ncores
-  Rscript classifier_repeat_wrapper.R --cancer_type $cancer_type --predictor $predictor --n_repeats 3 --ncores $ncores
+  Rscript classifier_repeat_wrapper.R --cancer_type $cancer_type --predictor $predictor --n_repeats 10 --ncores $ncores
 fi
 
 # Run the unsupervised analyses using subtype models
 if [ $predictor == "subtype" ]; then
-#  Rscript 4-ica_pca_feature_reconstruction.R --cancer_type $cancer_type --predictor $predictor --n_components 50
-#  Rscript 5-predict_category_reconstructed_data.R --cancer_type $cancer_type --predictor $predictor
-#  Rscript 6-save_recon_error_kappa_data.R --cancer_type $cancer_type --predictor $predictor
+  Rscript 4-ica_pca_feature_reconstruction.R --cancer_type $cancer_type --predictor $predictor --n_components 50
+  Rscript 5-predict_category_reconstructed_data.R --cancer_type $cancer_type --predictor $predictor
+  Rscript 6-save_recon_error_kappa_data.R --cancer_type $cancer_type --predictor $predictor
   Rscript 7-extract_plier_pathways.R --cancer_type $cancer_type --ncores $ncores --permute FALSE
   Rscript 7-extract_plier_pathways.R --cancer_type $cancer_type --ncores $ncores --permute TRUE
 fi
